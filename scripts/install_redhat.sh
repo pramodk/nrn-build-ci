70f2f04a3be5c98ff76806275f367c6bd400d179
Olli Lupton <oliver.lupton@epfl.ch>
2021-06-07 10:14:03 +0200

    Update RedHat distributions before installing. (#14)

--- scripts/install_redhat.sh
+++ scripts/install_redhat.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Use DNF if available (not CentOS7), otherwise YUM
 CMD=$(command -v dnf || command -v yum)
+${CMD} update -y
 ${CMD} install -y bison cmake diffutils dnf flex gcc gcc-c++ git \
   openmpi-devel libXcomposite-devel libXext-devel make ncurses-devel \
   ninja-build python3-devel python3-pip python3-wheel sudo which
