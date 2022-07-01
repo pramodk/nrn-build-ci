abf9d63c24acd0546b9516eebfc44a5cbca05627
Olli Lupton <oliver.lupton@epfl.ch>
2022-07-01 19:13:29 +0200

    Install a newer Flex on centos:7. (#43)

--- scripts/install_redhat_centos_7.sh
+++ scripts/install_redhat_centos_7.sh
@@ -12,3 +12,13 @@ alternatives --install /usr/local/bin/cmake cmake /usr/bin/cmake3 20 \
   --slave /usr/local/bin/ctest ctest /usr/bin/ctest3 \
   --slave /usr/local/bin/cpack cpack /usr/bin/cpack3 \
   --slave /usr/local/bin/ccmake ccmake /usr/bin/ccmake3
+
+# Install a newer version of Flex; this logic is copied from that in the
+# Dockerfile that is used to build the NEURON Python wheels.
+curl -o flex-2.6.4-9.el9.src.rpm http://mirror.stream.centos.org/9-stream/AppStream/source/tree/Packages/flex-2.6.4-9.el9.src.rpm
+yum-builddep -y flex-2.6.4-9.el9.src.rpm
+yum install -y rpm-build
+rpmbuild --rebuild flex-2.6.4-9.el9.src.rpm
+ls -R ${HOME}/rpmbuild
+yum install -y ${HOME}/rpmbuild/RPMS/x86_64/*.rpm
+flex --version
