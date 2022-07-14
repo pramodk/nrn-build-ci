44307c9ebcd5233eb6981e60c6a923fcf85c8f96
Olli Lupton <oliver.lupton@epfl.ch>
2022-07-14 11:11:11 +0200

    debian:buster: do not install Boost (#47)
    * Reverts #45 for the debian:buster image.
    * Implicitly reverts from OpenMPI to MPICH.

--- scripts/install_debian.sh
+++ scripts/install_debian.sh
@@ -8,9 +8,11 @@ export DEBIAN_FRONTEND=noninteractive
 # --- End GitHub-Actions-specific code ---
 # ---
 apt-get update
-apt-get install -y bison cmake flex git libboost-all-dev \
-  libncurses-dev libmpich-dev libssl-dev libx11-dev \
-  libxcomposite-dev ninja-build mpich libreadline-dev sudo wget unzip
+apt-get install -y bison cmake flex git libncurses-dev libmpich-dev libssl-dev \
+  libx11-dev libxcomposite-dev ninja-build mpich libreadline-dev sudo wget unzip
+if [[ "${DO_NOT_INSTALL_BOOST}" != "true" ]]; then
+  apt-get install -y libboost-all-dev
+fi
 if [[ -z "${NRN_PYTHON}" ]]; then
   apt-get install -y python3-dev python3-venv
   export NRN_PYTHON=$(command -v python3)
--- scripts/install_debian_debian_buster.sh
+++ scripts/install_debian_debian_buster.sh
@@ -2,3 +2,4 @@
 echo 'deb http://deb.debian.org/debian buster-backports main' >> /etc/apt/sources.list
 apt-get update
 apt-get install -t buster-backports -y cmake
+DO_NOT_INSTALL_BOOST=true
